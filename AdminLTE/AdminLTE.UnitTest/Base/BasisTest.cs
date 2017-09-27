14f971e307d41d02d03e07be355fa8f97e5c76e7
kiaser <flykiaser@163.com>
2017-09-28 00:04:38 +0800

    expressionTree

--- AdminLTE/AdminLTE.UnitTest/Base/BasisTest.cs
+++ AdminLTE/AdminLTE.UnitTest/Base/BasisTest.cs
@@ -20,5 +20,44 @@ namespace AdminLTE.UnitTest.Base
             
             WrileList(new List<string> {"1","2","3" }, new List<string> {"4","5","6" });
         }
+
+        [Test]
+        public void WriteMessages_return_true()
+        {
+            List<MyTestClass> list = new List<MyTestClass>
+            {
+                new MyTestClass {Id = "1", Name = "1"},
+                new MyTestClass {Id = "2", Name = "2"},
+                new MyTestClass {Id = "3", Name = "3"},
+                new MyTestClass {Id = "4", Name = "4"},
+                new MyTestClass {Id = "5", Name = "5"},
+            };
+
+            WriteMessages(list);
+        }
+
+        [Test]
+        public void ReflectionTest_get_propery_value()
+        {
+            MyTestClass myTestClass = new MyTestClass
+            {
+                Id = "1",
+                Name = "2"
+            };
+
+            Type t = myTestClass.GetType();
+
+            foreach (var propertyInfo in t.GetProperties())
+            {
+                Console.WriteLine(propertyInfo.Name);
+                //Console.WriteLine(propertyInfo.Name+" : "+ propertyInfo.GetValue(myTestClass,null));
+            }
+        }
+    }
+
+   public class MyTestClass
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
     }
 }
---
+++ AdminLTE/AdminLTE.UnitTest/Other/ExpressionTreeTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using AdminLTE.UnitTest.Core;
+using NUnit.Framework;
+
+namespace AdminLTE.UnitTest.Other
+{
+    [TestFixture]
+    public class ExpressionTreeTest : BaseTest
+    {
+
+        /// <summary>
+        /// Expression<Func<int, bool>> lambda = num => num < 5
+        ///https://docs.microsoft.com/zh-cn/dotnet/csharp/programming-guide/concepts/expression-trees/
+        /// </summary>
+        [Test]
+        public void Test1()
+        {
+            ParameterExpression numParam = Expression.Parameter(typeof(int), "num");
+            ConstantExpression five = Expression.Constant(5, typeof(int));
+
+            BinaryExpression numLessThanFive = Expression.LessThan(numParam, five);
+
+            Expression<Func<int, bool>> lambda1 =
+                Expression.Lambda<Func<int, bool>>(numLessThanFive, numParam);
+
+            Console.WriteLine(lambda1);
+
+
+
+        }
+
+        [Test]
+        public void Test2()
+        {
+            ParameterExpression value = Expression.Parameter(typeof(int), "value");
+
+            ParameterExpression result = Expression.Parameter(typeof(int), "result");
+
+            LabelTarget label = Expression.Label(typeof(int));
+
+            BlockExpression block = Expression.Block(new[] {result}, Expression.Assign(result, Expression.Constant(1)),
+                Expression.Loop(
+                    Expression.IfThenElse(
+                        Expression.GreaterThan(value, Expression.Constant(1)),
+                        Expression.MultiplyAssign(result, Expression.PostDecrementAssign(value)),
+                        Expression.Break(label, result)
+                    ),
+                    label
+                ));
+
+            int factorial = Expression.Lambda<Func<int, int>>(block, value).Compile()(5);
+
+            Console.WriteLine(factorial);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Expression<Func<int, bool>> exprTree = num => num < 5;
+
+            ParameterExpression param = (ParameterExpression)exprTree.Parameters[0];
+            BinaryExpression operation = (BinaryExpression) exprTree.Body;
+
+            ParameterExpression left = (ParameterExpression) operation.Left;
+            ConstantExpression right = (ConstantExpression) operation.Right;
+
+            Console.WriteLine("Decomposed expression: {0} => {1} {2} {3}",param.Name, left.Name,operation.NodeType,right.Value);
+
+            //Expression.Invoke()
+
+        }
+
+
+    }
+}
